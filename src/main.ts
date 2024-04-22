7ff6b90a19044454641f6434a5780597a76f81e3
zheng <3289474839@qq.com>
2024-04-22 21:19:59 +0800

    提交

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+// 导入svg
+import 'virtual:svg-icons-register'
+import App from '@/App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus, {
+    locale: zhCn,
+})
+app.mount('#app')
