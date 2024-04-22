9450b33c851cdd5667663921910c904770052864
test001 <24@qq.com>
2024-04-22 20:38:52 +0800

    svg小练习

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,10 @@ import {Plus} from '@element-plus/icons-vue'
     <div class="example-pagination-block">
     <div class="example-demonstration">When you have more than 7 pages</div>
     <el-pagination layout="prev, pager, next" :total="1000" />
+    <svg>>
+      <!-- xlink:href执行用哪一个图标，属性值务必是#icon -->
+      <use xlink:href="#icon-phone"></use>
+    </svg>
   </div>
   </div>
 </template>
