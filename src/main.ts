cf388d34f55bcac582b08294c36e0614abaa50f1
test001 <24@qq.com>
2024-04-22 20:28:41 +0800

    配置svg图标

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
+// 导入svg
+import 'virtual:svg-icons-register'
 import App from '@/App.vue'
 
 const app = createApp(App)
