021f806ccda0dd442e497bf556b547d3a17b8968
zhangdd <3289474839@qq.com>
2024-04-23 16:03:34 +0800

    mock假接口

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,40 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // 引入svg图标
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+// 引入mock插件提供方法
+import { viteMockServe } from 'vite-plugin-mock'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(),
-  // 配置svg图标
-  createSvgIconsPlugin({
-    // Specify the icon folder to be cached
-    iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-    // Specify symbolId format
-    symbolId: 'icon-[dir]-[name]',
-  }),
-  ],
-  // 全局路径设置为@
-  resolve: {
-    alias: {
-      "@": path.resolve("./src") // 相对路径别名配置，使用 @ 代替 src
-    }
-  },
-  // 设置scss全局变量文件
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: '@import "./src/styles/variable.scss";',
+export default defineConfig((command) => {
+  return {
+
+    plugins: [vue(),
+    // 配置svg图标
+    createSvgIconsPlugin({
+      // Specify the icon folder to be cached
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // Specify symbolId format
+      symbolId: 'icon-[dir]-[name]',
+    }),
+    viteMockServe({
+      localEnabled: command === 'serve',
+    }),
+    ],
+    // 全局路径设置为@
+    resolve: {
+      alias: {
+        "@": path.resolve("./src") // 相对路径别名配置，使用 @ 代替 src
+      }
+    },
+    // 设置scss全局变量文件
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/style/variable.scss";',
+        },
       },
     },
-  },
+
+  }
+
 })
